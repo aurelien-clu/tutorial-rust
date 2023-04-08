ff3e388de42dccea6ccf7fa9622aab4a68db5259
Aurelien Somename <aurelien.somename@gmail.com>
2023-04-08 21:22:28 +0200

    wip

--- crates/auth-jwt-server/src/auth/mod.rs
+++ crates/auth-jwt-server/src/auth/mod.rs
@@ -4,5 +4,29 @@ mod keys;
 mod model;
 
 pub use errors::AuthError;
+use jsonwebtoken::{encode, Header};
 pub use keys::KEYS;
 pub use model::{AuthBody, AuthPayload, Claims, Keys};
+
+pub fn authorize(payload: AuthPayload) -> Result<AuthBody, AuthError> {
+    // Check if the user sent the credentials
+    if payload.client_id.is_empty() || payload.client_secret.is_empty() {
+        return Err(AuthError::MissingCredentials);
+    }
+    // Here you can check the user credentials from a database
+    if payload.client_id != "foo" || payload.client_secret != "bar" {
+        return Err(AuthError::WrongCredentials);
+    }
+    let claims = Claims {
+        sub: "b@b.com".to_owned(),
+        company: "ACME".to_owned(),
+        // Mandatory expiry time as UTC timestamp
+        exp: 2000000000, // May 2033
+    };
+    // Create the authorization token
+    let token = encode(&Header::default(), &claims, &KEYS.encoding)
+        .map_err(|_| AuthError::TokenCreation)?;
+
+    // Send the authorized token
+    Ok(AuthBody::new(token))
+}
--- crates/auth-jwt-server/src/routes.rs
+++ crates/auth-jwt-server/src/routes.rs
@@ -1,6 +1,5 @@
 use crate::auth;
 use axum::Json;
-use jsonwebtoken::{encode, Header};
 
 pub async fn protected(claims: auth::Claims) -> Result<String, auth::AuthError> {
     // Send the protected data to the user
@@ -13,24 +12,8 @@ pub async fn protected(claims: auth::Claims) -> Result<String, auth::AuthError> 
 pub async fn authorize(
     Json(payload): Json<auth::AuthPayload>,
 ) -> Result<Json<auth::AuthBody>, auth::AuthError> {
-    // Check if the user sent the credentials
-    if payload.client_id.is_empty() || payload.client_secret.is_empty() {
-        return Err(auth::AuthError::MissingCredentials);
+    match auth::authorize(payload) {
+        Ok(auth_body) => Ok(Json(auth_body)),
+        Err(err) => Err(err),
     }
-    // Here you can check the user credentials from a database
-    if payload.client_id != "foo" || payload.client_secret != "bar" {
-        return Err(auth::AuthError::WrongCredentials);
-    }
-    let claims = auth::Claims {
-        sub: "b@b.com".to_owned(),
-        company: "ACME".to_owned(),
-        // Mandatory expiry time as UTC timestamp
-        exp: 2000000000, // May 2033
-    };
-    // Create the authorization token
-    let token = encode(&Header::default(), &claims, &auth::KEYS.encoding)
-        .map_err(|_| auth::AuthError::TokenCreation)?;
-
-    // Send the authorized token
-    Ok(Json(auth::AuthBody::new(token)))
 }
