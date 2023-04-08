947e0ca1273646f91578ffe19e2a0f9ec748523e
Aurelien Somename <aurelien.somename@gmail.com>
2023-04-08 21:25:39 +0200

    wip

--- crates/auth-jwt-server/src/routes.rs
+++ crates/auth-jwt-server/src/routes.rs
@@ -12,8 +12,5 @@ pub async fn protected(claims: auth::Claims) -> Result<String, auth::AuthError> 
 pub async fn authorize(
     Json(payload): Json<auth::AuthPayload>,
 ) -> Result<Json<auth::AuthBody>, auth::AuthError> {
-    match auth::authorize(payload) {
-        Ok(auth_body) => Ok(Json(auth_body)),
-        Err(err) => Err(err),
-    }
+    Ok(Json(auth::authorize(payload)?))
 }
